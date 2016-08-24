1465ef4621d8ad909623ffdf2823122964360c9c
Tyler Lam <tylerlam888@gmail.com>
2016-08-23 21:15:08 -0400

    Added carousel to testimonial section

--- js/custom-jquery.js
+++ js/custom-jquery.js
@@ -22,5 +22,30 @@ var main = function () {
 			$('.navbar').css('background', 'transparent');
 		}
 	});
+	//testimonails chevrons
+	var first = $(".testimonial-section .testimonials .carousel .first");
+	var last = $(".testimonial-section .testimonials .carousel .last"); 
+	//left chevron function
+	$("#chevron-left").click( function() {
+		var current = $(".testimonial-section .testimonials .carousel .active"); 
+		var previous = current.prev(".testimonial");
+		current.removeClass("active").hide();
+		if (current[0] == first[0]) {
+			last.fadeIn(1000).addClass("active");
+		} else {
+			previous.fadeIn(1000).addClass("active");
+		}
+	}); 
+	//right chevron function 
+	$("#chevron-right").click(function() {
+		var current = $(".testimonial-section .testimonials .carousel .active"); 
+		var next = current.next(".testimonial"); 
+		current.removeClass("active").hide(); 
+		if (current[0] == last[0]) {
+			first.fadeIn(1000).addClass("active");
+		} else {
+			next.fadeIn(1000).addClass("active");
+		}
+	});
 }
 $(document).ready(main);
